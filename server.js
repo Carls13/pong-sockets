53f660ada5ac76ef3bafa4eaba443d7f7eb73940
Carlos Hernández <charlesshb98@gmail.com>
2023-10-08 21:56:28 -0400

    feat: finished project

---
+++ server.js
@@ -0,0 +1,19 @@
+const apiServer = require("./api");
+const { listen } = require("./sockets");
+const http = require('http');
+
+const io = require("socket.io")
+
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = 3000;
+httpServer.listen(PORT);
+console.log(`Listening on port ${PORT}`);
+
+listen(socketServer);
